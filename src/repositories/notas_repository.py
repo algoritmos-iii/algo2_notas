bb9f161dc5ffbc80483ebdc9b788ad5f181216e6
Josue Bouchard <jbouchard@fi.uba.ar>
2021-10-28 11:31:20 -0300

    rewrite of notas repository
    * Created a base class `DevolucionBase`
    * Replaced Grupo with `DevolucionDeGrupo`
    * Replaced dataclasses with TypedDicts
    * Created `examenes` method
    * Modularized `ejercicios`
    * Formated code with black

--- src/repositories/notas_repository.py
+++ src/repositories/notas_repository.py
@@ -1,24 +1,36 @@
 from __future__ import annotations
 import gspread
 import gspread.utils
+from itertools import zip_longest
 
-from typing import TYPE_CHECKING, Tuple
-from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Tuple, List, Callable
+    from typing import Any, Iterable, Tuple, List, TypedDict, Dict, Callable
     from gspread.models import Worksheet
     from ..api.google_credentials import GoogleCredentials
 
 
-@dataclass(frozen=True)
-class Grupo:
+class FeedbackSpreadsheetData(TypedDict):
+    data: Dict[str, Any]
+    email_sent_row: int
+    sheet: Worksheet
+
+
+class DevolucionBase(TypedDict):
+    corrector: str
+    nota: str
+    detalle: str
+    mark_email_sent: Callable[[str], None]
+
+
+class DevolucionDeGrupo(DevolucionBase):
     numero: str
     emails: List[str]
-    corrector: str = field(repr=False)
-    nota: str = field(repr=False)
-    detalle: str = field(repr=False)
-    mark_email_sent: Callable[[str], None] = field(repr=False)
+
+
+class DevolucionDeExamen(DevolucionBase):
+    email: str
 
 
 class NotasRepository:
@@ -38,10 +50,10 @@ class NotasRepository:
         self._spreadsheet_key = spreadsheet_key
         self._google_credentials = credentials
 
-
     def _get_spreadsheet(self):
         client = gspread.authorize(
-            self._google_credentials.get_credenciales_spreadsheet())
+            self._google_credentials.get_credenciales_spreadsheet()
+        )
         spreadsheet = client.open_by_key(self._spreadsheet_key)
         return spreadsheet
 
@@ -53,8 +65,7 @@ class NotasRepository:
         return spreadsheet.worksheet(worksheet_name)
 
     def verificar(self, padron_web: str, email_web: str) -> bool:
-        """Verifica que hay un alumno con el padrón y e-mail indicados.
-        """
+        """Verifica que hay un alumno con el padrón y e-mail indicados."""
         alumnos = self._get_sheet(self.SHEET_ALUMNOS)
 
         for alumno in alumnos.get_all_records():
@@ -64,8 +75,10 @@ class NotasRepository:
             if not email or not padron:
                 continue
 
-            if (padron.lower() == padron_web.lower() and
-                    email.lower() == email_web.lower()):
+            if (
+                padron.lower() == padron_web.lower()
+                and email.lower() == email_web.lower()
+            ):
                 return True
 
         return False
@@ -90,59 +103,98 @@ class NotasRepository:
         words = exercise.strip().split(" ")
         words_lowercased = [word.lower() for word in words]
         spreadsheet_exercise_name = "_".join(words_lowercased)
-        named_range_name = f"{self.PREFIJO_RANGO_DEVOLUCIONES}_{spreadsheet_exercise_name}"
+        named_range_name = (
+            f"{self.PREFIJO_RANGO_DEVOLUCIONES}_{spreadsheet_exercise_name}"
+        )
         return named_range_name
-        
 
-    def ejercicios(self, ejercicio: str) -> List[Grupo]:
+    def _table_to_dict(self, table: Iterable):
+        """
+        Receives a list of lists where the first element is the Headers of the table
+        and the rest are the data.
+        Returns a tuple with the headers and a dict with data and the headers.
+        """
+        headers = table.pop(0)
+        data = [dict(zip_longest(headers, element, fillvalue="")) for element in table]
+        return data
+
+    def _get_feedback_data(self, hoja: str, ejercicio: str) -> FeedbackSpreadsheetData:
         exercise_named_range = self._exercise_name_to_named_range(ejercicio)
 
-        sheet = self._get_sheet(self.SHEET_DEVOLUCIONES)
-        emails, correcciones = sheet.batch_get(
-            [f"{self.RANGO_EMAILS}", exercise_named_range],
-            major_dimension="COLUMNS")
+        sheet = self._get_sheet(hoja)
+        batch = sheet.batch_get(
+            ranges=[f"{self.RANGO_EMAILS}", exercise_named_range],
+            major_dimension="COLUMNS",
+        )
 
-        # Construct headers
-        email_headers = emails.pop(0)
-        correciones_headers = correcciones.pop(0)
+        data = [{**self._table_to_dict(elem1), **self._table_to_dict(elem2)} for elem1, elem2 in zip(batch[0], batch[1])]
 
-        raw_data = [[
-            *emails[i],
-            *[""] * (len(email_headers) - len(emails[i])),
-            *correcciones[i],
-            *[""] * (
-                (len(email_headers) + len(correciones_headers)) -
-                (len(emails[i]) + len(correcciones[i]))
-            )
-        ] for i in range(len(correcciones))]
-
-        correciones_range = correcciones.range.split("!")[1]
+        correciones_range = batch[1].range.split("!")[1]
         # email_sent_row is zero indexed
         email_sent_row = gspread.utils.a1_range_to_grid_range(correciones_range)[
-            'endRowIndex']
+            "endRowIndex"
+        ]
 
-        grupos = []
-        EMPTY_CHARS_FIELD = ["#N/A", "#¡REF!", "", "0"]
-        for g in filter(lambda elem: elem[1] not in EMPTY_CHARS_FIELD and elem[5] in EMPTY_CHARS_FIELD, raw_data):
-            func: Callable[[int], Callable[[str], None]] = (
-                lambda group_number: (
-                    lambda value="True": sheet.update_cell(
-                        row=email_sent_row,
-                        col=group_number + 1,
-                        value=value
-                    )
-                )
-            )
+        return {"data": data, "email_sent_row": email_sent_row, "sheet": sheet}
 
-            grupos.append(
-                Grupo(
-                    numero=g[0],
-                    emails=g[1].split(","),
-                    nota=g[2],
-                    corrector=g[3],
-                    detalle=g[4],
-                    mark_email_sent=func(int(g[0]))
-                )
-            )
+    def _mark_email_sent_wrapper(
+        self, sheet: Worksheet, row: int, col: int
+    ) -> Callable[[str], None]:
+        return lambda value: sheet.update_cell(row=row, col=col + 1, value=value)
+
+    def _filter_field_not_empty(
+        fields_which_cant_be_empty: Iterable[str],
+    ) -> Callable[[Dict[str, Any]], bool]:
+        EMPTY_CHARS_SET = {"#N/A", "#¡REF!", "", "0"}
+
+        def _filter_criteria(elem: Dict[str, Any]):
+            fields_contents = set(elem[field] for field in fields_which_cant_be_empty)
+            return any(EMPTY_CHARS_SET.intersection(fields_contents))
+
+        return _filter_criteria
+
+    def examenes(self, examen: str) -> List[DevolucionDeExamen]:
+        filter_func = self._filter_field_not_empty({"Email", "Corrector", "Detalle"})
+
+        feedback = self._get_feedback_data(
+            hoja=self.SHEET_DEVOLUCIONES, ejercicio=examen
+        )
+
+        grupos: List[DevolucionDeGrupo] = [
+            {
+                "emails": grupo["Email"],
+                "corrector": grupo["Corrector"],
+                "detalle": grupo["Detalle"],
+                "nota": grupo["Nota"],
+                "mark_email_sent": self._mark_email_sent_wrapper(
+                    sheet=feedback["sheet"], row=feedback["email_sent_row"], col=index
+                ),
+            }
+            for index, grupo in enumerate(filter(filter_func, feedback["data"]))
+        ]
+
+        return grupos
+
+    def ejercicios(self, ejercicio: str) -> List[DevolucionDeGrupo]:
+        filter_func = self._filter_field_not_empty(
+            {"Grupo", "Email", "Corrector", "Detalle"}
+        )
+        feedback = self._get_feedback_data(
+            hoja=self.SHEET_DEVOLUCIONES, ejercicio=ejercicio
+        )
+
+        grupos: List[DevolucionDeGrupo] = [
+            {
+                "numero": grupo["Grupo"],
+                "emails": [email.strip() for email in grupo["Emails"].split(",")],
+                "corrector": grupo["Corrector"],
+                "detalle": grupo["Detalle"],
+                "nota": grupo["Nota"],
+                "mark_email_sent": self._mark_email_sent_wrapper(
+                    sheet=feedback["sheet"], row=feedback["email_sent_row"], col=index
+                ),
+            }
+            for index, grupo in enumerate(filter(filter_func, feedback["data"]))
+        ]
 
         return grupos
