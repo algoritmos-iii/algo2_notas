b403ffd5741e0cc29be593cd5272c915c9b4f2d5
Josue Bouchard <jbouchard@fi.uba.ar>
2021-11-01 09:34:10 -0300

    added linting script
    * added a linting script that exits with error code if pylint score is
    below threshold

---
+++ scripts/lint.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import argparse
+from pylint import lint
+
+"""
+This scripts lints all files given and exits with status code 1 if pylint score is less
+than threshold.
+
+Common uses:
+
+* Lint all python files:
+`python src/lint.py --threshold 7 $(find . -name '*.py')`
+
+* Lint all staged files:
+`python src/lint.py --threshold 7 $(git diff --name-only --cached)`
+
+
+"""
+
+parser = argparse.ArgumentParser(description='Lint application')
+parser.add_argument("--threshold", metavar='threshold', type=float, nargs=1, required=True,
+                    help='lowest threshold accepted')
+parser.add_argument("files", metavar='files', nargs='+', type=str,
+                    help="files to be linted")
+args = parser.parse_args()
+
+run = lint.Run(args.files, do_exit=False)
+score = run.linter.stats['global_note']
+
+if score < args.threshold[0]:
+    sys.exit(1)
