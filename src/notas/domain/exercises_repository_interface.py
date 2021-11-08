664c7759b4dbd171ac889dfcfae8e02669c2e39a
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2021-11-08 09:26:50 -0300

    renamed list method into get and changed return type

--- src/notas/domain/exercises_repository_interface.py
+++ src/notas/domain/exercises_repository_interface.py
@@ -1,13 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
-from .correction import GroupCorrection
+from .correction import GroupCorrectionCollection, GroupCorrection
 
 
 class ExerciseRepositoryInterface(ABC):
-
     @abstractmethod
-    def list(self) -> List[GroupCorrection]:
+    def get(self) -> List[GroupCorrectionCollection]:
         ...
 
     @abstractmethod
