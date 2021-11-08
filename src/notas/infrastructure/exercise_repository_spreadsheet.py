5d1e07ffcd0227cae8eaa3a1fb96b0d227835909
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2021-11-08 09:29:48 -0300

    first full version of ExerciseRepositorySpreadsheet

--- src/notas/infrastructure/exercise_repository_spreadsheet.py
+++ src/notas/infrastructure/exercise_repository_spreadsheet.py
@@ -1,39 +1,92 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-from .spreadsheet_repository_base import SpreadsheetRepositoryBase, spreadsheet_raw_data_to_dict
+from .spreadsheet_repository_base import (
+    SpreadsheetRepositoryBase,
+    spreadsheet_raw_data_to_dict,
+)
 from ..domain.exercises_repository_interface import ExerciseRepositoryInterface
-from ..domain.correction import GroupCorrection, Correction
+from ..domain.correction import (
+    GroupCorrectionCollection,
+    GroupCorrection,
+    Correction,
+    GroupSendingInformation,
+)
 
 if TYPE_CHECKING:
     from typing import List, Optional
 
 
 def _string_to_snake_case(string: str) -> str:
-    return string.lower().replace(' ', '_')
+    return string.lower().replace(" ", "_")
 
 
 def _exercise_to_named_range(exercise_name: str) -> str:
-    return 'emails_' + _string_to_snake_case(exercise_name)
+    return "emails_" + _string_to_snake_case(exercise_name)
 
 
-class ExerciseRepositorySpreadsheet(SpreadsheetRepositoryBase, ExerciseRepositoryInterface):
+class ExerciseRepositorySpreadsheet(
+    ExerciseRepositoryInterface, SpreadsheetRepositoryBase
+):
 
     WORKSHEET_NAME: str = "Devoluciones"
+    EMAIL_RANGE: str = "emailsGrupos"
 
-    def __init__(self, spreadsheet_credentials, spreadsheet_key: str) -> None:
-        super().__init__(spreadsheet_credentials, spreadsheet_key)
+    EXERCISES = [
+        "NPCs",
+        "Codigo Repetido",
+        "Números",
+        "Stack",
+        "Mars Rover",
+        "Servicios Financieros",
+        "Servicios Financieros 2",
+    ]
+
+    def __init__(self, service_account_credentials: dict, spreadsheet_key: str) -> None:
+        super().__init__(service_account_credentials, spreadsheet_key)
 
         self._worksheet = self._get_worksheet(self.WORKSHEET_NAME)
 
-    def list(self) -> List[GroupCorrection]:
-        ...
+    def get(self) -> List[GroupCorrectionCollection]:
+        ranges = [self.EMAIL_RANGE] + [
+            _exercise_to_named_range(exercise) for exercise in self.EXERCISES
+        ]
+        emails_raw, *correcciones_raw = self._worksheet.batch_get(
+            ranges, major_dimension="COLUMNS"
+        )
+
+        emails = spreadsheet_raw_data_to_dict(emails_raw)
+        corrections = [
+            spreadsheet_raw_data_to_dict(correccion_raw)
+            for correccion_raw in correcciones_raw
+        ]
+
+        return [
+            GroupCorrectionCollection(
+                group=GroupSendingInformation(
+                    group_number=emails[group_idx]["Grupo"],
+                    emails=emails[group_idx]["Emails"].split(","),
+                ),
+                corrections=[
+                    Correction(
+                        exercise_name=self.EXERCISES[correction_idx],
+                        grade=corrections[correction_idx][group_idx]["Nota"],
+                        corrector_name=corrections[correction_idx][group_idx][
+                            "Corrector"
+                        ],
+                        details=corrections[correction_idx][group_idx]["Detalle"],
+                    )
+                    for correction_idx in range(len(corrections))
+                ],
+            )
+            for group_idx in range(len(emails))
+            if not emails[group_idx]["Emails"] == ""
+        ]
 
     def find(self, exercise_name: str) -> Optional[GroupCorrection]:
         exercise_range = _exercise_to_named_range(exercise_name)
         emails_raw, correcciones_raw = self._worksheet.batch_get(
-            [f"{self.RANGO_EMAILS}", exercise_range],
-            major_dimension="COLUMNS"
+            [self.EMAIL_RANGE, exercise_range], major_dimension="COLUMNS"
         )
 
         emails = spreadsheet_raw_data_to_dict(emails_raw)
@@ -41,14 +94,17 @@ class ExerciseRepositorySpreadsheet(SpreadsheetRepositoryBase, ExerciseRepositor
 
         return [
             GroupCorrection(
-                group_number=email['Grupo'],
-                emails=email['Emails'],
+                group=GroupSendingInformation(
+                    group_number=email["Grupo"],
+                    emails=email["Emails"].split(","),
+                ),
                 correction=Correction(
                     exercise_name=exercise_name,
-                    grade=correccion['Nota'],
-                    corrector_name=correccion['Corrector'],
-                    details=correccion['Detalle'],
-                )
+                    grade=correccion["Nota"],
+                    corrector_name=correccion["Corrector"],
+                    details=correccion["Detalle"],
+                ),
             )
             for email, correccion in zip(emails, correcciones)
+            if not email["Emails"] == ""
         ]
