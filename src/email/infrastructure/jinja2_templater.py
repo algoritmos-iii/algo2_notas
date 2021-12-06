7703fdc96d08988478b97f12bfb234ba9ea48e7b
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2021-12-06 12:31:35 -0300

    added as_grade_str filter

--- src/email/infrastructure/jinja2_templater.py
+++ src/email/infrastructure/jinja2_templater.py
@@ -8,12 +8,17 @@ def markdown2HTML(value: str) -> str:
     return markdown.markdown(value, extensions=["fenced_code"])
 
 
+def as_grade_str(value: float) -> str:
+    return "%.2g".format(value)
+
+
 class Jinja2Templater(TemplaterInterface):
     def __init__(self, templates_search_path: str) -> None:
         self._jinja_env = Environment(
             loader=loaders.FileSystemLoader(templates_search_path)
         )
         self._jinja_env.filters["md"] = markdown2HTML
+        self._jinja_env.filters["as_grade_str"] = as_grade_str
 
     def render(self, template_name: str, context: dict) -> str:
         return self._jinja_env.get_template(template_name).render(context)
