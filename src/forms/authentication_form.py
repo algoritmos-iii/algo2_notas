6bb4f864aa88b6cafc326732f1a83c99e5493606
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2022-02-02 16:51:59 -0300

    autoformated and removed html5 dependency

--- src/forms/authentication_form.py
+++ src/forms/authentication_form.py
@@ -2,25 +2,25 @@ import flask_wtf
 
 from wtforms import fields
 from wtforms import validators
-from wtforms.fields import html5
 
 
 class AuthenticationForm(flask_wtf.FlaskForm):
-    """Pide el padrón y la dirección de correo.
-    """
-    padron = fields.StringField(
-        "Padrón", validators=[
-            validators.Regexp(r"\w+", message="Ingrese un padrón válido")])
+    """Pide el padrón y la dirección de correo."""
 
-    email = html5.EmailField(
-        "E-mail", validators=[
-            validators.Email(message="Ingrese una dirección de e-mail válida")])
+    padron = fields.StringField(
+        "Padrón",
+        validators=[validators.Regexp(r"\d+", message="Ingrese un padrón válido")],
+    )
+
+    email = fields.EmailField(
+        "E-mail",
+        validators=[validators.Email(message="Ingrese una dirección de e-mail válida")],
+    )
 
     submit = fields.SubmitField("Obtener enlace")
 
     def _normalize_field(self, field: fields.Field) -> str:
-        """Devuelve los datos del campo en minúsculas y sin espacio alrededor.
-        """
+        """Devuelve los datos del campo en minúsculas y sin espacio alrededor."""
         return field.data.strip().lower()
 
     def normalized_padron(self) -> str:
