3d155015de78786fa8c07e69952dbae05587f2dd
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2022-10-18 20:50:31 -0300

    added papers support

--- src/dataupdater/updater.py
+++ src/dataupdater/updater.py
@@ -5,6 +5,10 @@ from src.config import MongoConfig, SpreadsheetConfig
 from .spreadsheet_utils import spreadsheet_to_dict, s_to_float, s_to_int_or_none
 
 
+def _filter_raw(fields: list[str], amount: int):
+    return len(fields) == amount and all([field.strip() != "" for field in fields])
+
+
 def update_all():
     spreadsheet = gspread.service_account_from_dict(
         SpreadsheetConfig().credentials
@@ -13,19 +17,23 @@ def update_all():
     mongo = MongoClient(MongoConfig().url)
     db = mongo["algo3_2c2022"]
 
-    students_raw, exercises_raw, exams_raw = spreadsheet.values_batch_get(
-        ["Listado!B:E", "test-app!A:F", "test-app!K:S"],
+    students_raw, exercises_raw, exams_raw, papers_raw = spreadsheet.values_batch_get(
+        ["Listado!B:E", "test-app!A:F", "test-app!K:S", "test-app!X:Y"],
         params={"majorDimension": "ROWS"},
     )["valueRanges"]
 
     students = spreadsheet_to_dict(students_raw["values"])
     exercises = spreadsheet_to_dict(
         exercises_raw["values"],
-        filter_func=lambda exercise: len(exercise) >= 6 and exercise[5].strip() != "",
+        filter_func=lambda exercise: _filter_raw(exercise, 6),
     )
     exams = spreadsheet_to_dict(
         exams_raw["values"],
-        filter_func=lambda exam: len(exam) >= 7 and exam[6].strip() != "",
+        filter_func=lambda exam: _filter_raw(exam, 9),
+    )
+    papers = spreadsheet_to_dict(
+        papers_raw["values"],
+        filter_func=lambda paper: _filter_raw(paper, 2),
     )
 
     # Filter and transform data
@@ -42,6 +50,16 @@ def update_all():
         exam["puntos_extra"] = s_to_float(exam["puntos_extra"])
         exam["nota_final"] = s_to_float(exam["nota_final"])
         exam["padron"] = s_to_int_or_none(exam["padron"])
+
+    papers = [
+        {
+            "title": paper["paper"],
+            "winners": [
+                int(padron) for padron in paper["winners"].split("\n") if padron != ""
+            ],
+        }
+        for paper in papers
+    ]
 
     # Update db
     with mongo.start_session() as session:
@@ -101,3 +119,18 @@ def update_all():
             print("Exams updated")
         else:
             print("No exams")
+
+        if papers:
+            db["papers"].bulk_write(
+                [
+                    pymongo.UpdateOne(
+                        filter={"title": paper["title"]},
+                        update={"$set": paper, "$setOnInsert": {"email_sent": False}},
+                        upsert=True,
+                    )
+                    for paper in papers
+                ]
+            )
+            print("Papers updated")
+        else:
+            print("No papers")
