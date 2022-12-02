45b3a25c7691e9e5b862412f298c03b73362c850
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2022-12-02 11:41:30 -0300

    added exams emails

--- src/blueprints/admin.py
+++ src/blueprints/admin.py
@@ -10,6 +10,7 @@ admin_blueprint = flask.Blueprint(
     template_folder="template",
 )
 
+
 @admin_blueprint.get("/updatedb")
 @auth_required
 def updatedb():
@@ -52,6 +53,58 @@ def create_exercise_email(feedback):
     )
 
 
+def create_exam_email(feedback):
+    email = feedback["estudiante"]["email"]
+
+    context = {
+        "examen": feedback["examen"],
+        "nombre": feedback["estudiante"]["nombre"],
+        "corrector": feedback["corrector"],
+        "correcciones": feedback["detalle"],
+        "nota": feedback["nota"],
+        "puntos_extras": feedback["puntos_extra"],
+        "nota_final": feedback["nota_final"],
+    }
+
+    return (
+        Email()
+        .set_recipients(email)
+        .set_subject(
+            f"Corrección de {feedback.exam_name} - Padrón {feedback.student_padron}"
+        )
+        .set_cc_to_lista_docente(True)
+        .set_plaintext_content_from_template(
+            template_name="emails/notas_examen_plain.html",
+            context=context,
+        )
+        .set_html_content_from_template(
+            template_name="emails/notas_examen.html",
+            context=context,
+        )
+    )
+
+
+# Streaming function
+def email_streaming_generator(feedbacks):
+    with smtp_connection() as connection:
+
+        for feedback in feedbacks:
+            email = create_exercise_email(feedback)
+            email_sent_error = ""
+            try:
+                connection.send_message(email.generate_email_message())
+            except Exception as e:
+                email_sent_error = str(e)
+            else:
+                _db["exercises"].update_one(
+                    filter={"_id": feedback["_id"]},
+                    update={"$set": {"email_sent": True}},
+                )
+
+            yield f"{{address: {email._headers['to']}, subject: {email._headers['subject']}, email_sent: {not bool(email_sent_error)}}}\n"
+    yield "Emails sent"
+
+
 # Endpoints
 @admin_blueprint.post("/emails/exercise/<exercise>/send")
 @auth_required
@@ -74,82 +127,33 @@ def send_exercise_email(exercise: str):
     if not feedbacks:
         return f"Ejercicio {exercise} no encontrado o ya enviado"
 
-    def generate():
-        with smtp_connection() as connection:
-
-            for feedback in feedbacks:
-                email = create_exercise_email(feedback)
-                email_sent_error = ""
-                try:
-                    connection.send_message(email.generate_email_message())
-                except Exception as e:
-                    email_sent_error = str(e)
-                else:
-                    _db["exercises"].update_one(
-                        filter={"_id": feedback["_id"]},
-                        update={"$set": {"email_sent": True}},
-                    )
-
-                yield f"{{address: {email._headers['to']}, subject: {email._headers['subject']}, email_sent: {not bool(email_sent_error)}}}\n"
-        yield "Emails sent"
-
-    return flask.Response(flask.stream_with_context(generate()))
+    return flask.Response(
+        flask.stream_with_context(email_streaming_generator(feedbacks))
+    )
 
 
-# def create_exam_email(feedback):
-#     email = data_mapper.student_by_padron(feedback.student_padron).email
-#     student = data_mapper.student_by_padron(feedback.student_padron)
-#     context = {
-#         "examen": feedback.exam_name,
-#         "nombre": student.full_name,
-#         "corrector": feedback.corrector,
-#         "correcciones": feedback.details,
-#         "nota": float(feedback.grade.replace(",", ".")),
-#         "puntos_extras": float(feedback.extra_points.replace(",", ".")),
-#         "nota_final": float(feedback.final_grade.replace(",", ".")),
-#     }
+@admin_blueprint.get("/emails/exam/<exam>/send")
+@auth_required
+def send_exam_email(exam: str):
+    feedbacks = _db["exams"].aggregate(
+        [
+            {"$match": {"examen": exam, "email_sent": False}},
+            {
+                "$lookup": {
+                    "from": "students",
+                    "localField": "padron",
+                    "foreignField": "padron",
+                    "as": "estudiante",
+                    "pipeline": [{"$project": {"email": 1, "name": 1}}],
+                }
+            },
+            {"$set": {"estudiante": {"$arrayElemAt": ["$estudiante", 0]}}},
+        ]
+    )
 
-#     return (
-#         Email()
-#         .set_recipients(email)
-#         .set_subject(
-#             f"Corrección de {feedback.exam_name} - Padrón {feedback.student_padron}"
-#         )
-#         .set_cc_to_lista_docente(True)
-#         .set_plaintext_content_from_template(
-#             template_name="emails/notas_examen_plain.html",
-#             context=context,
-#         )
-#         .set_html_content_from_template(
-#             template_name="emails/notas_examen.html",
-#             context=context,
-#         )
-#     )
+    if not feedbacks:
+        return f"Examen {exam} no encontrado o ya enviado"
 
-
-# @admin_blueprint.get("/emails/exam/<exam>/send")
-# @auth_required
-# def send_exam_email(exam: str):
-#     feedbacks = data_mapper.not_sent_exam_feedback_by_name(exam)
-#     if not feedbacks:
-#         return f"Examen {exam} no encontrado o ya enviado"
-
-#     def generate():
-#         with smtp_connection() as connection:
-#             for feedback in feedbacks:
-#                 email = create_exam_email(feedback)
-#                 email_sent_error = ""
-#                 try:
-#                     connection.send_message(email.generate_email_message())
-#                 except Exception as e:
-#                     email_sent_error = str(e)
-#                 # else:
-#                 #     data_mapper.write_to_exam_sheet(
-#                 #         cell=feedback.email_sent_position,
-#                 #         value="TRUE",
-#                 #     )
-
-#                 yield f"{{address: {email._headers['to']}, subject: {email._headers['subject']}, email_sent: {not bool(email_sent_error)}}}\n"
-#         yield "Emails sent"
-
-#     return flask.Response(flask.stream_with_context(generate()))
+    return flask.Response(
+        flask.stream_with_context(email_streaming_generator(feedbacks))
+    )
